6eb83bb087bf874148fddb8c72e623c256ca8c78
Daniel Laurentino <daniel.s.laurent@gmail.com>
2017-09-26 15:13:33 -0300

    feat: Added API util and antd usage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import './styles/App.css'
+import * as ReadableAPI from './utils/ReadableAPI'
+
 
 class App extends Component {
+
+  componentDidMount() {
+    ReadableAPI.getCategories().then(categories => (
+      console.log(categories)
+    ))
+    ReadableAPI.getCategoryPosts('redux').then(catPosts => (
+      console.log(catPosts)
+    ))
+    ReadableAPI.getPosts().then(posts => (
+      console.log(posts)
+    ))
+  }
   render() {
+    const { Header, Content, Footer } = Layout;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '12px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Readable ©2017 by @danlaurent
+          </Footer>
+        </Layout>
       </div>
     );
   }
